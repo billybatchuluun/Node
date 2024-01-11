43da222cfd7a75688729c168f36858f4ad316c37
Bilguun Batchuluun <23LP6327@G304-CC0073.local>
2024-01-11 17:48:02 +0800

    update

--- file.js
+++ file.js
@@ -7,25 +7,34 @@ const create = (data) => {
     }
     const previousData = JSON.parse(json);
     previousData.push(data);
-    console.log(previousData);
+    // console.log(previousData);
   });
 };
 
-// create({
-//   id: 11,
-//   first_name: "Bilguun",
-//   last_name: "Batchuluun",
-//   email: "Billy0@storify.com",
-//   gender: "Male",
-//   ip_address: "43.66.94.194",
-// });
+create({
+  id: 11,
+  first_name: "Bilguun",
+  last_name: "Batchuluun",
+  email: "Billy0@storify.com",
+  gender: "Male",
+  ip_address: "43.66.94.194",
+});
 
-const update = (id, data) => {
-  fs.readFile('imput.json', 'utf-8', (err, json)=>{
-    if(err) return console.error(err);
+// const update = (id, data) => {
+//   fs.readFile('imput.json', 'utf-8', (err, json)=>{
+//     if(err) return console.error(err);
 
-    const previousData-JSON.parse(json);
-    const previousData.find((element)=>element.id===id)
-  })
+//     const previousData-JSON.parse(json);
+//     const previousData.find((element)=>element.id===id)
+//   })
+// };
+
+const delet = (id) => {
+  fs.readFile("input.json", "utf-8", (err, json) => {
+    if (err) console.log("errror:", err);
+    const data = JSON.parse(json);
+    const newArr = data.filter((info) => info.id !== id);
+    console.log(newArr);
+  });
 };
-
+delet(2);
