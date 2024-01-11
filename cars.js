002522a03182c3daccc881c6d9bfc65b897c5e00
Bilguun Batchuluun <23LP6327@G304-CC0073.local>
2024-01-11 17:37:50 +0800

    update

--- cars.js
+++ cars.js
@@ -1,12 +1,44 @@
-const fs = require("node:fs");
+const fs = require("fs");
+const http = require("http");
+// const { url } = require("node:inspector");
 
-const createUsers = (data) => {
-  fs.readFile("Cars.json", "utf-8", function (err, data) {
-    if (err) {
-      return console.error(err);
-    }
-    const previousData = JSON.parse(json);
-    previousData.push(data);
-    console.log(previousData);
-  });
-};
+const server = http.createServer((req, res) => {
+  const reqUrl = req.url;
+  if (reqUrl == "/") {
+    return (
+      res.writeHead(200, { "Content-Type": "text/html" }),
+      res.end("<h1>Welcome dude</h1>")
+    );
+  }
+  if (reqUrl == "/users") {
+    return fs.readFile("input.json", "utf-8", (err, json) => {
+      if (err) console.log("error:", err);
+      const data = JSON.parse(json);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(data));
+    });
+  }
+  if (reqUrl == "/cars") {
+    return fs.readFile("cars.json", "utf-8", (err, json) => {
+      if (err) console.log("error:", err);
+      const data = JSON.parse(json);
+      res.writeHead(200, { "Content-Type": "application/json" });
+      res.end(JSON.stringify(data));
+    });
+  }
+});
+
+// const createUsers = (data) => {
+//   fs.readFile("Cars.json", "utf-8", function (err, data) {
+//     if (err) {
+//       return console.error(err);
+//     }
+//     const previousData = JSON.parse(json);
+//     previousData.push(data);
+//     console.log(previousData);
+//   });
+// };
+const PORT = 6060;
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
