1095ae7070405910e0a3abd4942e8b1b8f4e56b0
Bilguun Batchuluun <23LP6327@G304-CC0073.local>
2024-01-16 16:34:17 +0800

    update

--- delete.js
+++ delete.js
@@ -4,8 +4,9 @@ const deleteData = (id) => {
   const json = fs.readFileSync("input.json", "utf-8");
   const data = JSON.parse(json);
   const newArray = data.filter((item) => {
-    item.id !== id;
+    return item.id !== id;
   });
   fs.writeFileSync("input.json", JSON.stringify(newArray));
+  return newArray;
 };
 exports.deleteData = deleteData;
